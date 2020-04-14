a73fa9e12d3e884a3c640dafc52f1c1085aa3572
Leonardo Ospizio <ospizio.1808991@studenti.uniroma1.it>
2020-04-15 00:42:01 +0200

    genera un token (puo essere acquisito con una chiamata AJAX (see:index.php) ) ho predisposto una funzione che prende un endpoint e ritorna un risultato (anch'esso puo` essere acquisito con ajax) [ AJAX + JQUERY e` bello e facile :) ]

---
+++ prova_token/index_.php
@@ -0,0 +1,66 @@
+<?php
+// [TODO] Copiare risultato del browser su http://json.parser.online.fr/
+
+/* Spotify Application Client ID and Secret Key */
+$client_id     = 'a351932160f74239bb1a1459e88819f7';
+$client_secret = 'cc456cf809e64324a526cc27fad02063';
+
+/* Get Spotify Authorization Token */
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,            'https://accounts.spotify.com/api/token' );
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1 );
+curl_setopt($ch, CURLOPT_POST,           1 );
+curl_setopt($ch, CURLOPT_POSTFIELDS,     'grant_type=client_credentials' );
+curl_setopt($ch, CURLOPT_HTTPHEADER,     array('Authorization: Basic '.base64_encode($client_id.':'.$client_secret)));
+
+$result=curl_exec($ch);
+echo $result;
+echo "<br>";
+$json = json_decode($result, true);
+curl_close($ch);
+
+$access_token = $json["access_token"];
+$token_type = $json["token_type"];
+
+/* Examples of usage */
+echo getResult("/v1/search?q=top&type=playlist&market=IT",$token_type, $access_token);
+echo "<hr>";
+//echo getResult("/v1/search?q=top%2020&type=playlist",$token_type, $access_token);
+//echo "<hr>";
+//echo getResult("/v1/search?q=global&type=playlist",$token_type, $access_token);
+//echo "<hr>";
+//echo getResult("/v1/search?q=carlbrave&type=playlist",$token_type, $access_token);
+//echo "<hr>";
+//echo getResult("/v1/browse/categories",$token_type, $access_token);
+//echo "<hr>";
+//echo getResult("/v1/browse/new-releases",$token_type, $access_token);
+//echo "<hr>";
+//echo getResult("/v1/recommendations",$token_type, $access_token);
+//echo "<hr>";
+//echo getResult("/v1/browse/featured-playlists",$token_type, $access_token);
+//echo "<hr>";
+
+/*
+    $json = json_decode($result,true); // true, create an associative array
+*/
+
+/* get a spotify _endpoint, a _token_type and an _access_token */
+/* return a JSON string with the query's result */
+/* [TODO] for simple decode go to http://json.parser.online.fr/ */
+
+function getResult($endpoint, $token_type, $access_token){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,            'https://api.spotify.com'.$endpoint );
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER,     array('Authorization: '.$token_type.' '.$access_token));
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+}
+
+
+
+
+
+
+?>
