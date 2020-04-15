3cb3b21e3824a26af1763e6f688fbf82dc783165
Francesco Riccardo Tassone <francescor.tassone@gmail.com>
2020-04-15 17:23:54 +0200

    Organizzazione codice

---
+++ scripts/utils.js
@@ -0,0 +1,9 @@
+
+
+function getPlaylistIds() {
+    return playlistsIds = ["37i9dQZEVXbIQnj7RRhdSX"];
+}
+
+function getToken() {
+    return token = "BQD6QwWfrtTQCqVrjvRBAjgezfhfuNslW0ND-2FL7KNMnVw7q71XYoo_6O1vBCGo7b9rAknDDFsmhEVJXAQ";
+}
