5dbae108c8d1827c4712db53b0df3d419a39c920
Francesco Riccardo Tassone <francescor.tassone@gmail.com>
2020-04-17 20:52:54 +0200

    Game graphic 0.2a

--- view/game.php
+++ view/game.php
@@ -21,20 +21,20 @@
     <div class="row">
         <div class="col-sm-6 my-3">
             <div class="media border">
-                <img class="align-self-center mr-3 h-25 w-25" id="cover0" src="https://i.scdn.co/image/ab67616d0000b27334874e17c6825f69edcefef5" >
+                <img class="align-self-center mr-3 h-25 w-25" id="cover0" src="./resources/white_square.png" alt="Cover">
                 <div class="media-body align-self-center">
-                    <h5 class="" id="titolo0">Titolo canzone</h5>
-                    <p id="artista0">Artistaaaaa fsajfsdihfbdsihishdbi</p>
+                    <h5 class="" id="titolo0"></h5>
+                    <p id="artista0"></p>
                     <a class="stretched-link" onclick="stopPlay(0)" href="#"></a>
                 </div>
             </div>
         </div>
         <div class="col-sm-6 my-3">
             <div class="media border" onclick="">
-                <img class="align-self-center mr-3 h-25 w-25" id="cover1" src="https://i.scdn.co/image/ab67616d0000b27334874e17c6825f69edcefef5" >
+                <img class="align-self-center mr-3 h-25 w-25" id="cover1" src="./resources/white_square.png" alt="Cover">
                 <div class="media-body align-self-center">
-                    <h5 class="" id="titolo1">Titolo canzone</h5>
-                    <p id="artista1">Album affsa</p>
+                    <h5 class="" id="titolo1"></h5>
+                    <p id="artista1"></p>
                     <a class="stretched-link" onclick="stopPlay(0)" href="#"></a>
                 </div>
             </div>
@@ -43,20 +43,20 @@
     <div class="row">
         <div class="col-sm-6 my-3">
             <div class="media border">
-                <img class="mr-3 h-25 w-25" id="cover2" src="https://i.scdn.co/image/ab67616d0000b27334874e17c6825f69edcefef5" >
+                <img class="mr-3 h-25 w-25" id="cover2" src="./resources/white_square.png" alt="Cover">
                 <div class="media-body align-self-center">
-                    <h5 class="" id="titolo2">Titolo canzone</h5>
-                    <p id="artista2">Album affsa</p>
+                    <h5 class="" id="titolo2"></h5>
+                    <p id="artista2"></p>
                     <a class="stretched-link" onclick="stopPlay(0)" href="#"></a>
                 </div>
             </div>
         </div>
         <div class="col-sm-6 my-3">
             <div class="media border">
-                <img class="mr-3 h-25 w-25" id="cover3" src="https://i.scdn.co/image/ab67616d0000b27334874e17c6825f69edcefef5" >
+                <img class="mr-3 h-25 w-25" id="cover3" src="./resources/white_square.png" alt="Cover">
                 <div class="media-body align-self-center">
-                    <h5 class="" id="titolo3">Titolo canzone</h5>
-                    <p id="artista3">Album affsa</p>
+                    <h5 class="" id="titolo3"></h5>
+                    <p id="artista3"></p>
                     <a class="stretched-link" onclick="stopPlay(0)" href="#"></a>
                 </div>
             </div>
