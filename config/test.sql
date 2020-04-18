33e4f8ed12349304bef2ba349d3471f1917b3b0c
Leonardo Ospizio <ospizio.1808991@studenti.uniroma1.it>
2020-04-18 17:43:10 +0200

    aggiunto schema db pulito (ho fatto delle modifiche senza testarle ma dovrebbe funzionare)

---
+++ config/test.sql
@@ -0,0 +1,89 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Creato il: Apr 18, 2020 alle 17:35
+-- Versione del server: 10.4.11-MariaDB
+-- Versione PHP: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(30) NOT NULL,
+  `descrizione` text NOT NULL,
+  `moltiplicatore` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Struttura della tabella `games`
+--
+
+CREATE TABLE `games` (
+  `id` int(11) NOT NULL,
+  `user` int(11) NOT NULL,
+  `categoria` int(11) NOT NULL,
+  `score` int(11) NOT NULL,
+  `numero_domande` int(2) NOT NULL,
+  `esatte` int(2) NOT NULL,
+  `errate` int(2) NOT NULL,
+  `mancate` int(2) NOT NULL,
+  `vittoria` tinyint(1) NOT NULL,
+  `start` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `stop` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Struttura della tabella `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `sesso` enum('M','F') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+--
+-- Indici per le tabelle `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nome` (`nome`);
+
+--
+-- Indici per le tabelle `games`
+--
+ALTER TABLE `games`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user` (`user`),
+  ADD KEY `categoria` (`categoria`);
+
+--
+-- Indici per le tabelle `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
