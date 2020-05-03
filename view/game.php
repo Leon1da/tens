06cb21dd92c30e2fdb293379f987dcdc028ce454
Francesco Riccardo Tassone <francescor.tassone@gmail.com>
2020-05-03 18:47:58 +0200

    Game graphic 1.0a

--- view/game.php
+++ view/game.php
@@ -24,117 +24,124 @@
 </div>
 
 <!-- Play e info -->
-<div class="row justify-content-center mt-3 mb-4" style="height: 16rem">
+<div class="row my-3 justify-content-center" style="height: 85vh">
     <div class="col-10">
-        <div class="row align-items-center rounded shadow-lg mx-auto h-100">
-            <!-- struttura tabs -->
-            <div class="col-3">
-                <div class="nav flex-column nav-pills" role="tablist" id="menu">
-                    <a class="nav-link active" id="istruzioni-tab" href="#istruzioni-cont" data-toggle="pill" role="tab">Istruzioni</a>
-                    <a class="nav-link" id="impostazioni-tab" href="#impostazioni-cont" data-toggle="pill" role="tab">Impostazioni</a>
-                    <a class="nav-link" id="partita-tab" href="#partita-cont" data-toggle="pill" role="tab">Partita</a>
+        <div class="row justify-content-center " style="height: 90% ; width: 100%">
+            <!-- Menu tabs -->
+            <div class="row" style="height: 10%; width: 100%">
+                <div class="col-12">
+                    <ul class="nav nav-pills" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link active" id="istruzioni-tab" href="#istruzioni-cont" data-toggle="pill" role="tab" aria-controls="nav-istruzioni" aria-selected="true">Istruzioni</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" id="partita-tab" href="#partita-cont" data-toggle="pill" role="tab" aria-controls="nav-partita" aria-selected="false">Partita</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
-            <!-- Contenuto -->
-            <div class="col-9 h-100 p-3">
-                <div class="tab-content h-75 pt-2">
-                    <div class="tab-pane fade show active" id="istruzioni-cont" role="tabpanel">
-                        Qui andranno le istruzioni carine e coccolose <br> <br> <br> <br> Anche qui
-                    </div>
 
-                    <!-- Tab impostazioni -->
-                    <div class="tab-pane fade h-100" id="impostazioni-cont" role="tabpanel">
-                        <div class="row justify-content-center h-75">
-                            <div class="col-12">
-                                <h2 class="text-center">Modalità</h2>
+
+            <!-- contenuto tabs -->
+            <div class="row justify-content-center" style="height: 90%; width: 100%">
+                <div class="col-12">
+                    <div class="tab-content">
+                        <!-- Istruzioni e impostazioni -->
+                        <div class="tab-pane fade show active" id="istruzioni-cont" role="tabpanel">
+                            Sint sit mollit irure quis est nostrud cillum consequat Lorem esse do quis dolor esse fugiat sunt do. Eu ex commodo veniam Lorem aliquip laborum occaecat qui Lorem esse mollit dolore anim cupidatat. Deserunt officia id Lorem nostrud aute id commodo elit eiusmod enim irure amet eiusmod qui reprehenderit nostrud tempor. Fugiat ipsum excepteur in aliqua non et quis aliquip ad irure in labore cillum elit enim. Consequat aliquip incididunt ipsum et minim laborum laborum laborum et cillum labore. Deserunt adipisicing cillum id nulla minim nostrud labore eiusmod et amet. Laboris consequat consequat commodo non ut non aliquip reprehenderit nulla anim occaecat. Sunt sit ullamco reprehenderit irure ea ullamco Lorem aute nostrud magna.
+                        </div>
+
+
+                        <!-- Partita -->
+                        <div class="tab-pane fade" id="partita-cont" role="tabpanel">
+                            <!-- Punteggio -->
+                            <div class="row justify-content-center h-25">
+                                <div class="col">
+                                    <div class="row justify-content-center h-75 mb-4">
+                                        <div class="col-12">
+                                            <h2 class="text-center" id="punteggio_testo">Totale</h2>
+                                            <h1 class="text-center" id="punteggio_valore">0</h1>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="col-10 col-sm-6 col-md-3">
-                                <select class="form-control" id="selettore_modalita"></select>
+
+
+                            <!-- Scelte -->
+                            <div class="row justify-content-center h-75">
+                                <div class="col-12">
+                                    <div class="row">
+                                        <div class="col-sm-6 my-1">
+                                            <div class="media rounded shadow-lg p-2 bg-light">
+                                                <img class="align-self-center mr-3 h-25 w-25 rounded" id="cover0" src="./resources/Transparent.png" alt="Cover">
+                                                <div class="media-body align-self-center">
+                                                    <h5 class="text-dark" id="titolo0"></h5>
+                                                    <p class="text-dark" id="artista0"></p>
+                                                    <a class="stretched-link" onclick="stopPlay(0)" href="#" disabled></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-6 my-1">
+                                            <div class="media rounded shadow-lg p-2 bg-light">
+                                                <img class="align-self-center mr-3 h-25 w-25 rounded" id="cover1" src="./resources/Transparent.png" alt="Cover">
+                                                <div class="media-body align-self-center">
+                                                    <h5 class="text-dark" id="titolo1"></h5>
+                                                    <p class="text-dark" id="artista1"></p>
+                                                    <a class="stretched-link" onclick="stopPlay(1)" href="#"></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-sm-6 my-1">
+                                            <div class="media rounded shadow-lg p-2 bg-light">
+                                                <img class="align-self-center mr-3 h-25 w-25 rounded" id="cover2" src="./resources/Transparent.png" alt="Cover">
+                                                <div class="media-body align-self-center">
+                                                    <h5 class="text-dark" id="titolo2"></h5>
+                                                    <p class="text-dark" id="artista2"></p>
+                                                    <a class="stretched-link" onclick="stopPlay(2)" href="#"></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-6 my-1">
+                                            <div class="media rounded shadow p-2 bg-light">
+                                                <img class="align-self-center mr-3 h-25 w-25 rounded" id="cover3" src="./resources/Transparent.png" alt="Cover">
+                                                <div class="media-body align-self-center">
+                                                    <h5 class="text-dark" id="titolo3"></h5>
+                                                    <p class="text-dark" id="artista3"></p>
+                                                    <a class="stretched-link" onclick="stopPlay(3)" href="#"></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
                             </div>
                         </div>
-                    </div>
-                    <div class="tab-pane fade" id="partita-cont" role="tabpanel">
-                        <div class="row justify-content-center h-75">
-                            <div class="col-12">
-                                <h2 class="text-center">Punteggio</h2>
-                            </div>
-                            <div class="col-12">
-                                <p class="text-center" id="punteggio_testo">Totale</p>
-                                <h2 class="text-center" id="punteggio_valore">0</h2>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-
-
-                <!-- zona pulsante e bar -->
-                <div class="row justify-content-center h-25">
-                    <div class="col-12">
-                        <div class="progress" style="height: 1px;">
-                            <div class="progress-bar progress-custom" id="progresso_round" role="progressbar" style="width: 100%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                    </div>
-                    <div class="col-10 col-sm-6 col-md-3 align-self-end">
-                        <button class="btn btn-primary btn-block" type="button" id="gioca_btn" disabled>
-                        </button>
                     </div>
                 </div>
             </div>
         </div>
+
+
+
+
+
+        <!-- Bar e pulsante gioca -->
+        <div class="row justify-content-center" style="height: 10%; width: 100%">
+            <div class="col-12">
+                <div class="progress" style="height: 1px;">
+                    <div class="progress-bar progress-custom" id="progresso_round" role="progressbar" style="width: 100%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+            <div class="col-10 col-sm-6 col-md-3 align-self-end">
+                <button class="btn btn-primary btn-block" type="button" id="gioca_btn" disabled>
+                </button>
+            </div>
+        </div>
     </div>
+
 </div>
-
-
-    <!-- Zona risposte -->
-    <div class="row justify-content-center h-50">
-        <div class="col-10">
-            <div class="row">
-                <div class="col-sm-6 my-1">
-                    <div class="media rounded shadow-lg p-2 bg-light">
-                        <img class="align-self-center mr-3 h-25 w-25 rounded" id="cover0" src="./resources/Transparent.png" alt="Cover">
-                        <div class="media-body align-self-center">
-                            <h5 class="text-dark" id="titolo0"></h5>
-                            <p class="text-dark" id="artista0"></p>
-                            <a class="stretched-link" onclick="stopPlay(0)" href="#" disabled></a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6 my-1">
-                    <div class="media rounded shadow-lg p-2 bg-light">
-                        <img class="align-self-center mr-3 h-25 w-25 rounded" id="cover1" src="./resources/Transparent.png" alt="Cover">
-                        <div class="media-body align-self-center">
-                            <h5 class="text-dark" id="titolo1"></h5>
-                            <p class="text-dark" id="artista1"></p>
-                            <a class="stretched-link" onclick="stopPlay(1)" href="#"></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-6 my-1">
-                    <div class="media rounded shadow-lg p-2 bg-light">
-                        <img class="align-self-center mr-3 h-25 w-25 rounded" id="cover2" src="./resources/Transparent.png" alt="Cover">
-                        <div class="media-body align-self-center">
-                            <h5 class="text-dark" id="titolo2"></h5>
-                            <p class="text-dark" id="artista2"></p>
-                            <a class="stretched-link" onclick="stopPlay(2)" href="#"></a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6 my-1">
-                    <div class="media rounded shadow p-2 bg-light">
-                        <img class="align-self-center mr-3 h-25 w-25 rounded" id="cover3" src="./resources/Transparent.png" alt="Cover">
-                        <div class="media-body align-self-center">
-                            <h5 class="text-dark" id="titolo3"></h5>
-                            <p class="text-dark" id="artista3"></p>
-                            <a class="stretched-link" onclick="stopPlay(3)" href="#"></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 <script>
     firstLoad();
 </script>
