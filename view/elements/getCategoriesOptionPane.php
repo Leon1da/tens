19f2ff78798beb33db931651804866d505461dbe
Leonardo Ospizio <ospizio.1808991@studenti.uniroma1.it>
2020-05-13 21:56:29 +0200

    piccoli update grafici

---
+++ view/elements/getCategoriesOptionPane.php
@@ -0,0 +1,26 @@
+<?php
+require('database.php');
+
+$query = "SELECT * FROM category";
+$check = $pdo->prepare($query);
+$check->execute();
+
+$categories = $check->fetchAll(PDO::FETCH_ASSOC);
+
+echo printOptionCategory($categories);
+
+
+// stampa il menu a tendina con tutte le categorie presenti nel database
+function printOptionCategory($categories){
+    $html = '<select id="category" class="form-control">';
+    $html.= '<option selected>All categories</option>';
+
+    foreach ($categories as $category){
+        $html.= '<option>'.$category["nome"].'</option>';
+    }
+
+    $html.= '</select>';
+    return $html;
+
+}
+?>
