bf9d5f9b1d082de2c42b7ded1231f8634a733f6f
Leonardo Ospizio <ospizio.1808991@studenti.uniroma1.it>
2020-05-14 14:34:10 +0200

    bug fix introdotto con l'aggiunta della libreria jquery ui (la libreria ora viene caricata dai cdn di google ;) )

--- index.php
+++ index.php
@@ -12,7 +12,9 @@
     <!-- Custom CSS -->
     <link rel="stylesheet" href="css/style.css?v27">
 
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+
+<!--    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">-->
 
     <title>Ten's</title>
 </head>
@@ -201,12 +203,9 @@
  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+ <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
- <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
- <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-
+ <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 
  <script src="js/model_view.js"></script>
 
@@ -239,11 +238,9 @@
                 case "#profile":
                     // mostro modal profilo
                     $("#profile-modal").removeClass("hide-my-profile-modal");
-
                     var url = "view/profile.php";
                     $("#profile-modal-body").load(url);
-
-                                break;
+                    break;
                 case "#logout":
                     var url = "model/logout.php";
                     $.get(url, function (response) {
