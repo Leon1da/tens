67f49017f45cc87176e2b8ac75ff81d9d53ba5ac
Leonardo Ospizio <ospizio.1808991@studenti.uniroma1.it>
2020-05-20 22:39:52 +0200

    ultimata pagina about-us bisogna correggere la tua matricola nella mail

--- view/about-us.php
+++ view/about-us.php
@@ -1,21 +1,31 @@
 
     <div class="row justify-content-center m-0">
-        <div class="card" style="width: 25rem; margin: 5rem;">
+        <div class="card" style="width: 25rem; margin: 5rem; background-color: transparent;">
             <img src="resources/hacker-vector-cartoon.png" class="card-img-top" style="width: 25rem; height: 25rem;" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+            <div class="card-body" style="background-color: white;">
+                <h5 class="card-title">Francesco Tassone</h5>
+                <p class="card-text">
+                    <a class="btn"href="https://www.instagram.com/francescotassone98/"><img src="resources/ig_icon.svg" alt="" width="20" height="20">&nbsp;&nbsp;&nbsp;@francesco.tassone</a>
+                    <a class="btn" href="https://www.facebook.com/francesco.tassone.127"><img src="resources/fb_icon.svg" alt="" width="20" height="20" >&nbsp;&nbsp;&nbsp;Francesco Tassone</a>
+                    <a class="btn" href="https://www.gmail.com"><img src="resources/gmail_icon.svg" alt="" width="20" height="20" >&nbsp;&nbsp;&nbsp;tassone.1808991@studenti.uniroma1.it </a>
+                </p>
             </div>
         </div>
 
-        <div class="card" style="width: 25rem; margin: 5rem;">
+        <div class="card" style="width: 25rem; margin: 5rem; background-color: transparent;">
             <img src="resources/hacker-vector-cartoon.png" class="card-img-top" style="width: 25rem; height: 25rem;" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+            <div class="card-body" style="background-color: white;">
+                <h5 class="card-title">Leonardo Ospizio</h5>
+                <p class="card-text">
+                    <a class="btn"href="https://www.instagram.com/leonardo.ospizio/"><img src="resources/ig_icon.svg" alt="" width="20" height="20">&nbsp;&nbsp;&nbsp;@leonardo.ospizio</a>
+                    <a class="btn" href="https://www.facebook.com/leonardo.ospizio"><img src="resources/fb_icon.svg" alt="" width="20" height="20" >&nbsp;&nbsp;&nbsp;Leonardo Ospizio</a>
+                    <a class="btn" href="https://www.gmail.com"><img src="resources/gmail_icon.svg" alt="" width="20" height="20" >&nbsp;&nbsp;&nbsp;ospizio.1808991@studenti.uniroma1.it </a>
+                </p>
             </div>
         </div>
-
     </div>
+    <div class="row justify-content-center m-0">
+        <p> Vuoi segnalarci un bug, proporci una collaborazione o offrirci una birra?
+        Contattaci, ti risponderemo il prima possibile.</p>
+    </div>
+
