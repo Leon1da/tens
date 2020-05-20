11882f0caf495868a7ec5faf17b6d8ea8d207fc6
Leonardo Ospizio <ospizio.1808991@studenti.uniroma1.it>
2020-05-20 22:47:17 +0200

    aggiunto attributo blank che fa aprire i link in un altra pagina

--- view/about-us.php
+++ view/about-us.php
@@ -5,9 +5,9 @@
             <div class="card-body" style="background-color: white;">
                 <h5 class="card-title">Francesco Tassone</h5>
                 <p class="card-text">
-                    <a class="btn"href="https://www.instagram.com/francescotassone98/"><img src="resources/ig_icon.svg" alt="" width="20" height="20">&nbsp;&nbsp;&nbsp;@francesco.tassone</a>
-                    <a class="btn" href="https://www.facebook.com/francesco.tassone.127"><img src="resources/fb_icon.svg" alt="" width="20" height="20" >&nbsp;&nbsp;&nbsp;Francesco Tassone</a>
-                    <a class="btn" href="https://www.gmail.com"><img src="resources/gmail_icon.svg" alt="" width="20" height="20" >&nbsp;&nbsp;&nbsp;tassone.1808991@studenti.uniroma1.it </a>
+                    <a class="btn"href="https://www.instagram.com/francescotassone98/" target="_blank"><img src="resources/ig_icon.svg" alt="" width="20" height="20">&nbsp;&nbsp;&nbsp;@francesco.tassone</a>
+                    <a class="btn" href="https://www.facebook.com/francesco.tassone.127" target="_blank"><img src="resources/fb_icon.svg" alt="" width="20" height="20" >&nbsp;&nbsp;&nbsp;Francesco Tassone</a>
+                    <a class="btn" href="https://www.gmail.com" target="_blank"><img src="resources/gmail_icon.svg" alt="" width="20" height="20" >&nbsp;&nbsp;&nbsp;tassone.1814263@studenti.uniroma1.it </a>
                 </p>
             </div>
         </div>
@@ -17,9 +17,9 @@
             <div class="card-body" style="background-color: white;">
                 <h5 class="card-title">Leonardo Ospizio</h5>
                 <p class="card-text">
-                    <a class="btn"href="https://www.instagram.com/leonardo.ospizio/"><img src="resources/ig_icon.svg" alt="" width="20" height="20">&nbsp;&nbsp;&nbsp;@leonardo.ospizio</a>
-                    <a class="btn" href="https://www.facebook.com/leonardo.ospizio"><img src="resources/fb_icon.svg" alt="" width="20" height="20" >&nbsp;&nbsp;&nbsp;Leonardo Ospizio</a>
-                    <a class="btn" href="https://www.gmail.com"><img src="resources/gmail_icon.svg" alt="" width="20" height="20" >&nbsp;&nbsp;&nbsp;ospizio.1808991@studenti.uniroma1.it </a>
+                    <a class="btn"href="https://www.instagram.com/leonardo.ospizio/" target="_blank"><img src="resources/ig_icon.svg" alt="" width="20" height="20">&nbsp;&nbsp;&nbsp;@leonardo.ospizio</a>
+                    <a class="btn" href="https://www.facebook.com/leonardo.ospizio" target="_blank"><img src="resources/fb_icon.svg" alt="" width="20" height="20" >&nbsp;&nbsp;&nbsp;Leonardo Ospizio</a>
+                    <a class="btn" href="https://www.gmail.com" target="_blank"><img src="resources/gmail_icon.svg" alt="" width="20" height="20" >&nbsp;&nbsp;&nbsp;ospizio.1808991@studenti.uniroma1.it </a>
                 </p>
             </div>
         </div>
